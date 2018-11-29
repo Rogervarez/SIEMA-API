a016574e5501ebf85858c5a1dcb858abd227bc93
Rogervarez <rogelioalvarez2@gmail.com>
2018-11-29 15:16:36 -0600

    Cambios en modelos
    Aun hay comentarios en los modelos que se tienen que implementar en el core

--- SIEMA/models.py
+++ SIEMA/models.py
@@ -18,11 +18,32 @@ class Demographics(models.Demographics):
     #falta traducir grupo etnico :( solo se puede por el momento en los archivos core de Opal
     
     pass
-class Location(models.Location): pass
+class Location(models.Location): 
+    category = fields.CharField(max_length=255, blank=True, verbose_name=("Categoria"))
+    hospital = fields.CharField(max_length=255, blank=True, verbose_name=("Hospital"))
+    ward = fields.CharField(max_length=255, blank=True, verbose_name=("Sala"))
+    bed = fields.CharField(max_length=255, blank=True, verbose_name=("Cama"))
+    pass
+class Diagnosis(models.Diagnosis): 
+    #falta traducir condition :( solo se puede por el momento en los archivos core de Opal
+    details = fields.CharField(max_length=255, blank=True, verbose_name=("Detalles"))
+    date_of_diagnosis = fields.DateField(verbose_name=("Fecha de diagnostico"))
+    pass
+class PastMedicalHistory(models.PastMedicalHistory): 
+    #falta traducir condition :( solo se puede por el momento en los archivos core de Opal
+    year = fields.CharField(max_length=255, blank=True, verbose_name=("Anio"))
+    details = fields.CharField(max_length=255, blank=True, verbose_name=("Detalles"))
+    pass
+class Treatment(models.Treatment): 
+    #falta traducir drug :( solo se puede por el momento en los archivos core de Opal
+    dose = fields.CharField(max_length=255, blank=True, verbose_name=("Dosis"))
+    #falta traducir route :( solo se puede por el momento en los archivos core de Opal
+    start_date = fields.DateField(verbose_name=("Fecha de Inicio"))
+    end_date = fields.DateField(verbose_name=("Fecha de Fin"))
+    #falta traducir frequency :( solo se puede por el momento en los archivos core de Opal
+    pass
+#De aqui hacia abajo ya no se neceita para el demo
 class Allergies(models.Allergies): pass
-class Diagnosis(models.Diagnosis): pass
-class PastMedicalHistory(models.PastMedicalHistory): pass
-class Treatment(models.Treatment): pass
 class Investigation(models.Investigation): pass
 class SymptomComplex(models.SymptomComplex): pass
 class PatientConsultation(models.PatientConsultation): pass
