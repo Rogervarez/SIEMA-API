bce4de8ffdb7cbcf7034a6fc9bcc2098e70d06e6
Sigfrid Gom <sig@dell>
2018-11-29 08:38:03 -0600

    Revert "Disque termine las traducciones de todo"
    This reverts commit d7a2c90fbff0c029521898b923fe91ab692494eb.

--- SIEMA/models.py
+++ SIEMA/models.py
@@ -9,15 +9,7 @@ from opal import models
 Core Opal models - these inherit from the abstract data models in
 opal.models but can be customised here with extra / altered fields.
 """
-
-class Demographics(models.Demographics):
-    first_name = fields.CharField(max_length=255, blank=True, verbose_name=("Nombre"))
-    surname = fields.CharField(max_length=255, blank=True, verbose_name=("Apellido"))
-    date_of_birth = fields.DateField(verbose_name=("Fecha de nacimiento"))
-    #falta traducir sexo :( solo se puede por el momento en los archivos core de Opal
-    #falta traducir grupo etnico :( solo se puede por el momento en los archivos core de Opal
-    
-    pass
+class Demographics(models.Demographics): pass
 class Location(models.Location): pass
 class Allergies(models.Allergies): pass
 class Diagnosis(models.Diagnosis): pass
